487ec671fb91a0d9723c6fa2ec561f1f4c744803
KelleyStover <kelleystover@yahoo.com>
2013-04-28 16:52:27 -0700

    added the question on everyone's mind

---
+++ Spork/src/spork/Spork.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spork;
+
+/**
+ *
+ * @author kelleystover
+ */
+public class Spork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Will Kitty Play With It?");
+    }
+}
